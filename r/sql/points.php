0578a7bfc74f39b19cad427fe8b14b9d8a0872d5
Ivan Maeder <ivan.maeder@gmail.com>
2014-07-27 10:12:31 +0200

    CREATE TABLE

--- r/sql/points.php
+++ r/sql/points.php
@@ -2,6 +2,23 @@
 
 namespace sql\points;
 
+/*
+
+--2014-07-27
+
+CREATE TABLE IF NOT EXISTS `points` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `coordinate` point NOT NULL,
+  `lat` decimal(10,8) NOT NULL,
+  `lng` decimal(11,8) NOT NULL,
+  `point` point NOT NULL,
+  `x` decimal(16,6) NOT NULL,
+  `y` decimal(16,6) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+*/
+
 function insert($lat, $lng, $x, $y) {
     $sql = "INSERT INTO points (
               coordinate,
